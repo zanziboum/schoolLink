734e7ffd442917b9b87596cfa18a012fdfc4ec5e
zanziboum <zanziboum.zanzi@orange.fr>
2023-04-17 15:40:11 +0200

    Initial commit

---
+++ src/main/java/fr/isep/schoolLink/SchoolLinkApplication.java
@@ -0,0 +1,13 @@
+package fr.isep.schoolLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/isep/schoolLink/SchoolLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isep.schoolLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
