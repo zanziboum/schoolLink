cf7cc4cb4157d40bdef9eb6be51167852f94fbad
jbouveret18 <you@example.com>
2023-05-22 17:59:24 +0200

    Arranged data sections split main in 3 functions

--- src/main/java/fr/isep/schoolLink/data/ReadJsonFile.java
+++ src/main/java/fr/isep/schoolLink/data/ReadJsonFile.java
@@ -5,38 +5,38 @@ import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
 
 import java.io.FileReader;
+import java.util.List;
 
 public class ReadJsonFile {
-    public static void main(String[] args) {
+    String filepath = "C:\\Users\\jbouv\\OneDrive\\Desktop\\Ecole\\ISEP\\Cycle_ingenieur\\A2\\Web Technologies\\Project\\src\\main\\java\\fr\\isep\\schoolLink\\data\\universities.json";
+    public static JSONArray readJSONFile(String filePath) {
         JSONParser parser = new JSONParser();
 
         try {
             // Read the JSON file
-            Object obj = parser.parse(new FileReader("C:\\Users\\jbouv\\OneDrive\\Desktop\\Ecole\\ISEP\\Cycle_ingenieur\\A2\\Web Technologies\\Project\\src\\main\\java\\fr\\isep\\schoolLink\\data\\universities.json"));
+            Object obj = parser.parse(new FileReader(filePath));
 
             // Convert the object to a JSON object
             JSONObject jsonObject = (JSONObject) obj;
 
             // Get the universities array
-            JSONArray universities = (JSONArray) jsonObject.get("universities");
-
-            // Iterate over each university
-            for (Object universityObj : universities) {
-                JSONObject university = (JSONObject) universityObj;
-
-                // Get the university information
-                String name = (String) university.get("name");
-                String specialtyDomain = (String) university.get("specialty_domain");
-                String country = (String) university.get("country");
-
-                // Print the university information
-                System.out.println("Name: " + name);
-                System.out.println("Specialty Domain: " + specialtyDomain);
-                System.out.println("Country: " + country);
-                System.out.println();
-            }
+            return (JSONArray) jsonObject.get("universities");
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        return null;
+    }
+
+    public static String getUniversityName(JSONObject university) {
+        return (String) university.get("name");
+    }
+
+    public static String getUniversitySpecialtyDomain(JSONObject university) {
+        return (String) university.get("specialty_domain");
+    }
+
+    public static String getUniversityCountry(JSONObject university) {
+        return (String) university.get("country");
     }
 }
