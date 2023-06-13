a3c1e920fca1b441f021b2ec10c2c12c881b548f
Max <73904781+Maximilien-de-la-Gastine@users.noreply.github.com>
2023-06-13 17:34:00 +0200

    link with chat page

--- front/javaScript/login_script.js
+++ front/javaScript/login_script.js
@@ -7,8 +7,6 @@ document.addEventListener("DOMContentLoaded", function() {
         const login = document.getElementById("emailInput").value;
         const password = document.getElementById("passwordInput").value;
 
-
-
         try {
             const response = await axios.post("http://localhost:8080/api/auth/login", {
                 email: login,
